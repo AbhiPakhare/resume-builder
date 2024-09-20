fbe67232828c402ba817e07ccb0c6f8ea8eedec1
Abhishek Pakhare <abhishekpakhare1997@gmail.com>
2024-09-21 01:05:02 +0530

    [Fix] : commented auth routes for nw

--- backend/Routes/AuthRoutes.js
+++ backend/Routes/AuthRoutes.js
@@ -1,9 +1,13 @@
-const { login, registerUser } = require("../controller/Authcontroller");
-const { userAuthValidation } = require("../middlewares/authValidation");
+// const { login, registerUser } = require("../controller/AuthController");
+// const { userAuthValidation } = require("../middlewares/authValidation");
 
 const router = require("express").Router();
 
-router.post("/register", userAuthValidation, registerUser);
-router.post("/login", userAuthValidation, login);
+router.get("/register", (req, res) => {
+  res.send("register");
+});
+
+// router.post("/register", userAuthValidation, registerUser);
+// router.post("/login", userAuthValidation, login);
 
 module.exports = router;
